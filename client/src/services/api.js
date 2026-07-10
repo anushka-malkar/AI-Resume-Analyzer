632c99475808432f5e1593944310c8b25fde8cbd
Anushka Malkar <malkaranushka07@gmail.com>
2026-07-10 15:35:14 +0530

    updated api url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://ai-resume-analyzer-eq9f.onrender.com/api",
+  baseURL: "https://ai-resume-analyzer-eq9f.onrender.com",
 });
 
 export default API;
